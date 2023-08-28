9d0d5dfe73791e9d6e5cde447a4644aefc53d0df
Andre Lucas <andrelucca99@gmail.com>
2023-08-28 15:54:59 -0300

    refactor: export object medalhas para paste utils

--- src/page/PageSobre.tsx
+++ src/page/PageSobre.tsx
@@ -7,6 +7,7 @@ import {
   useTheme,
 } from "@mui/material";
 import { LayoutBaseDePagina } from "../shared/layouts";
+import { medalhas } from "../utils/medalhas";
 
 export const PageSobre: React.FC = () => {
   const theme = useTheme();
@@ -14,36 +15,36 @@ export const PageSobre: React.FC = () => {
   const mdDown = useMediaQuery(theme.breakpoints.down("md"));
   const lgDown = useMediaQuery(theme.breakpoints.down("lg"));
 
-  const medalhas = [
-    {
-      imagem: "https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png",
-      name: "HTML",
-    },
-    {
-      imagem:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/250px-CSS3_logo.svg.png",
-      name: "CSS",
-    },
-    {
-      imagem:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
-      name: "JavaScript",
-    },
-    {
-      imagem:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
-      name: "REACT",
-    },
-    {
-      imagem: "https://testing-library.com/img/octopus-128x128.png",
-      name: "Testing Library",
-    },
-    {
-      imagem:
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
-      name: "TypeScript",
-    },
-  ];
+  // const medalhas = [
+  //   {
+  //     imagem: "https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png",
+  //     name: "HTML",
+  //   },
+  //   {
+  //     imagem:
+  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/250px-CSS3_logo.svg.png",
+  //     name: "CSS",
+  //   },
+  //   {
+  //     imagem:
+  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+  //     name: "JavaScript",
+  //   },
+  //   {
+  //     imagem:
+  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+  //     name: "REACT",
+  //   },
+  //   {
+  //     imagem: "https://testing-library.com/img/octopus-128x128.png",
+  //     name: "Testing Library",
+  //   },
+  //   {
+  //     imagem:
+  //       "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+  //     name: "TypeScript",
+  //   },
+  // ];
 
   return (
     <LayoutBaseDePagina titulo="Sobre">
---
+++ src/utils/medalhas.tsx
@@ -0,0 +1,30 @@
+export const medalhas = [
+  {
+    imagem: "https://www.w3.org/html/logo/downloads/HTML5_Badge_512.png",
+    name: "HTML",
+  },
+  {
+    imagem:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/6/62/CSS3_logo.svg/250px-CSS3_logo.svg.png",
+    name: "CSS",
+  },
+  {
+    imagem:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1200px-Unofficial_JavaScript_logo_2.svg.png",
+    name: "JavaScript",
+  },
+  {
+    imagem:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png",
+    name: "REACT",
+  },
+  {
+    imagem: "https://testing-library.com/img/octopus-128x128.png",
+    name: "Testing Library",
+  },
+  {
+    imagem:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4c/Typescript_logo_2020.svg/1200px-Typescript_logo_2020.svg.png",
+    name: "TypeScript",
+  }
+];
